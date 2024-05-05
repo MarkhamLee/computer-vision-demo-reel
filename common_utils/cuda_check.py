36c54c1748d1b6408a8af1fd1d82d03e5571adfa
Markham Lee <markhamdev@markhamlee.com>
2024-05-04 23:44:26 -0700

    Simple script for checking for cuda availability without having to run a model

---
+++ common_utils/cuda_check.py
@@ -0,0 +1,14 @@
+import sys
+import os
+import torch
+
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(parent_dir)
+
+from common_utils.logging_utils import LoggingUtilities  # noqa: E401
+
+logger = LoggingUtilities.console_out_logger()
+
+# device = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+logger.info(f'Running on device: {device}')
